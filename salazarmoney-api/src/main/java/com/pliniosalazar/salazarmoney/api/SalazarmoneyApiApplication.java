8b0988231af55d01897856dcc5949a9f9c1ec6dc
kanttanhed <plinio.salazar@yahoo.com.br>
2022-10-18 16:45:14 -0300

    initial project

---
+++ salazarmoney-api/src/main/java/com/pliniosalazar/salazarmoney/api/SalazarmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.pliniosalazar.salazarmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalazarmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalazarmoneyApiApplication.class, args);
+	}
+
+}
---
+++ salazarmoney-api/src/test/java/com/pliniosalazar/salazarmoney/api/SalazarmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pliniosalazar.salazarmoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalazarmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
